971002ba325cd9f689bc9202df5042e684c710df
Mrinal Purohit <github@mrinalpurohit.in>
2017-06-07 13:01:02 +0530

    Change bot logic to work only for DM's
    It's not required to tag a bot in a channel or add the bot to the channel. Bot
    will work when you send a private message to it.

--- start_bot.py
+++ start_bot.py
@@ -18,24 +18,32 @@ def split_bot_tag(message):
         return message[len(bot_tag) + 1:]
 
 
+def is_dm(channel_id):
+    return channel_id[0] == "D"
+
+
 def parse_data(slack_rtm_data):
     if slack_rtm_data:
         for messages_obj in slack_rtm_data:
-            if messages_obj and "text" in messages_obj:
-                msg_content = split_bot_tag(messages_obj["text"])
+            if (messages_obj and "text" in messages_obj
+                    and "bot_id" not in messages_obj
+                    and is_dm(messages_obj["channel"])):
+                msg_content = messages_obj["text"]
                 from_user = messages_obj["user"]
                 date = time.strftime("%d-%m-%Y",
                                      time.localtime(float(messages_obj["ts"])))
-                return from_user, msg_content, date
-    return None, None, None
+                channel = messages_obj["channel"]
+                return from_user, msg_content, date, channel
+    return None, None, None, None
 
 
 def main():
     if slack_client.rtm_connect():
         print("report_tracker connected and running")
         while True:
-            from_user, message, date = parse_data(slack_client.rtm_read())
-            if from_user and message and date:
+            from_user, message, date, channel = parse_data(
+                slack_client.rtm_read())
+            if from_user and message and date and channel:
                 print(from_user, message, date)
                 save_data(from_user, message, date)
             time.sleep(1)
