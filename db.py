60b377a1b9ffce02d6453963c46e33640d005d1d
Mrinal Purohit <github@mrinalpurohit.in>
2017-06-15 23:42:52 +0530

    Migrate to postgres from sqlite

--- db.py
+++ db.py
@@ -1,22 +1,26 @@
 import logging
 import os
-import sqlite3 as sql
+import psycopg2 as sql
 import sys
 
 
-table_name = os.environ.get("SQLITE_TABLE")
+table_name = os.environ.get("DB_TABLE")
 
 try:
-    con = sql.connect(os.environ.get("SQLITE_DB"))
+    con = sql.connect(host=os.environ.get("DB_HOST"),
+                      database=os.environ.get("DB_NAME"),
+                      user=os.environ.get("DB_USER"),
+                      password=os.environ.get("DB_PASSWORD"))
     c = con.cursor()
     table_create = """
 create table if not exists {0}(
 channel text,
 user_id text,
-timestamp text,
+timestamp timestamp,
 message text
 );""".format(table_name)
     c.execute(table_create)
+    con.commit()
 except sql.Error as e:
     logging.error(e.args[0], exc_info=True)
     sys.exit(1)
@@ -26,6 +30,6 @@ def save_data(channel, user, content, date):
     logging.debug("channel:{0} user:{1} timestamp:{2} content:{3}".format(
         channel, user, date, content))
     insert = """insert into {0} (channel, user_id, timestamp, message)
- values (?, ?, ?, ?)""".format(table_name)
+ values (%s, %s, to_timestamp(%s), %s)""".format(table_name)
     c.execute(insert, (channel, user, date, content))
     con.commit()
